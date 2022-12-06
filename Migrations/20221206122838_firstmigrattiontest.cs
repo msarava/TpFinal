a3aee0fe6f8053798ba3653f6bca7285e9de2fd2
lucas jimenez <pro.lucasjimenez@gmail.com>
2022-12-06 13:54:39 +0100

    add migration

---
+++ Migrations/20221206122838_firstmigrattiontest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeBonCoinnew.Migrations
+{
+    /// <inheritdoc />
+    public partial class firstmigrattiontest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
