40876d0116f8954f59b4fcd84e22a549f9f43d24
Arnaud DEPETRIS <arnaud.depetris@gmail.com>
2022-12-06 12:04:45 +0100

    avant migration

---
+++ Tools/DataBaseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LeBonCoin_Toulouse.Tools
+{
+    public class DataBaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=tcp:utopios-m2i.database.windows.net,1433;Initial Catalog=leboncoin_m2i_toulouse;Persist Security Info=False;User ID=m2iformation;Password=Toto.Tata12/;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
+    }
+}
