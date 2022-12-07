b03a2bc622dabf20e70c17ea3d3937993c5dcfc4
Arnaud DEPETRIS <arnaud.depetris@gmail.com>
2022-12-07 11:35:29 +0100

    Changemetn dans progromme.cs pour l'accessibilité swagger

--- Program.cs
+++ Program.cs
@@ -60,11 +60,10 @@ builder.Services.AddAuthorization((builder) =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 app.UseCors("all");
